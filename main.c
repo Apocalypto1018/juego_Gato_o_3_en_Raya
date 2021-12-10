eb30bc129ff7744fe82eb22881d846686a19ec12
Apocalypto1018 <ricardo.medina.d123@gmail.com>
2021-12-09 23:55:01 -0400

    funciones multiplataforma

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-#include <conio.h>
 #include <string.h>
+#include <time.h>
 
 #define COL 18
 #define FIL 6
 
 //prototipos de las funciones
-void gotoxy(int, int);
 void inicio();
 void jugar();
 int tablero(int[9], int[9]);
@@ -21,6 +19,10 @@ int posValidas(int, int[9], int[9], int, int);
 
 void vaciar(int[9]);
 
+void limP();
+void pausa();
+void clean_stdin(void);
+
 int main(){
 
 	//variables locales
@@ -29,22 +31,21 @@ int main(){
 	
 	//ciclo principal del juego
 	do{
-		system("cls"); //limpiar pantalla
+		limP(); //limpiar pantalla
 		printf("          Juego Del Gato\n\n");
 		inicio();
 		printf("\n\n\n*Ingrese 1 para jugar\n\n");
 		printf("*Ingrese 0 para salir\n->");
-		fflush(stdin); //se limpia el buffer
 		scanf("%i", &opc);
 		
-		system("cls"); //limpiar pantalla
+		clean_stdin();
+		limP(); //limpiar pantalla
 		
 		switch(opc){//estudio de casos para ejecutar la solicitud del usuario sobre jugar o salir
 			
 			case 0:{
 				printf("Saliendo...\n");	
 				salir=1;
-				
 				break;
 			}
 				
@@ -55,7 +56,7 @@ int main(){
 					
 			default:{
 					printf("Parametro ingresado al menu no valido!\n");
-					system("pause");
+					pausa();
 					break;
 			}	
 			
@@ -65,70 +66,34 @@ int main(){
 	return 0;
 }
 	
-//funcion que posiciona un objeto en x e y
-void gotoxy(int x, int y) {
-	HANDLE hCon;
-	hCon=GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD dwPos;
-	dwPos.X=x;
-	dwPos.Y=y;
-	SetConsoleCursorPosition(hCon,dwPos);
-}
-
-	
 //funcion que pinta un tablero para el inicio
 void inicio(){
 	
-	int i,j;
+	int i=0,j=0;
 	
-	//marco izquierdo
-	gotoxy(4,3); printf("%c",124);
-	gotoxy(4,4); printf("%c",46);
-	gotoxy(4,5); printf("%c",124);
-	gotoxy(4,6); printf("%c",46);
-	gotoxy(4,7); printf("%c",124);
+	//cargar matriz
+	char matriz[FIL][COL]={ 
+		"     [0] [1] [2] ",
+		"[0] |   |   |   |",
+		"    .............",
+		"[1] |   |   |   |",
+		"    .............",
+		"[2] |   |   |   |",
+	};
 	
-	//marco derecho
-	gotoxy(16,3); printf("%c",124);
-	gotoxy(16,4); printf("%c",46);
-	gotoxy(16,5); printf("%c",124);
-	gotoxy(16,6); printf("%c",46);
-	gotoxy(16,7); printf("%c",124);
-	
-	//indicadores izquierda
-	gotoxy(0,3); printf("[0]");
-	gotoxy(0,5); printf("[1]");
-	gotoxy(0,7); printf("[2]");
-	
-	//indicadores superiores
-	gotoxy(5,2); printf("[0]");
-	gotoxy(9,2); printf("[1]");
-	gotoxy(13,2); printf("[2]");
-	
-	//lineas centrales verticales
-	for (i=3; i<8; i++) {
-		gotoxy(8,i);
-		printf("%c",124);
-	}
-	for (i=3; i<8; i++) {
-		gotoxy(12,i);
-		printf("%c",124);
-	}
-	
-	//lineas centrales horizontales
-	for (i=4; i<16; i++) {
-		gotoxy(i,4);
-		printf("%c",46);
-	}
-	for (i=4; i<16; i++) {
-		gotoxy(i,6);
-		printf("%c",46);
+	for(i=0;i<FIL;i++){
+		for(j=0;j<COL;j++){	
+			printf("%c",matriz[i][j]);
+		}
+		printf("\n");
 	}
 	
 }
 
 //funcion que ejecutara las rutinas del juego al iniciarse
 void jugar(){
+	srand(time(0)); // seteando la semilla
+		
 	//cadenas para almacenar el nombre
 	char jugador1[25]; 
 	char jugador2[25];
@@ -157,11 +122,14 @@ void jugar(){
 	int ganador=2;//variable de control que alerta si hay un ganador
 	
 	int posAux=0; //varaible auxiliar que serivira para saber si la posiciones ya fue ocupada
+	
+	int jugarPC=0; //variable de control para saber si va a jugar la pc (1 para si, cero para no)
+	
+	int papel=0; //variable de control, si se desea jugar en papel
 
 	do{
 		//preguntar cuantos jugadores
 		printf("*Ingrese la cantidad de jugadores 1/2\n->");
-		fflush(stdin);
 		scanf("%i", &nJugadores);
 		
 		if(nJugadores<1 || nJugadores>2)
@@ -169,24 +137,24 @@ void jugar(){
 		
 	} while(nJugadores<1 || nJugadores>2); //ciclo que asegura que se ingrese una cantidad correcta de jugadores
 	
-	//system("cls"); //borrar pantalla
+	clean_stdin();
 	
 	if(nJugadores==2){
 		//solicitud de los nombres de los jugadores
 		printf("\n*Ingrese el nombre del primer jugador\n->");
-		fflush(stdin); //se limpia el buffer
 		gets(jugador1);
 		
 		//pedir caracter para el jugador 1
 		do{
 			printf("*Jugador 1 Ingrese el caracter a jugar entre x/o\n->");
-			fflush(stdin);
 			scanf("%c", &caracter1);
 		
 			if(caracter1!='x' && caracter1!='o')
 				printf("Solo puede elegir entra x/o para jugar...\n");
 		
 		} while(caracter1!='x' && caracter1!='o');
+		
+		clean_stdin();
 		
 		//se le da el caracter del jugador 2 dependiendo de la eleccion del jugador 1
 		if(caracter1=='x')
@@ -195,29 +163,33 @@ void jugar(){
 			caracter2='x';
 		
 		printf("\n*Ingrese el nombre del segundo jugador\n->");
-		fflush(stdin); //se limpia el buffer
 		gets(jugador2);
 		
 		printf("\nJugador 2, su caracter es: %c\n", caracter2);
-		system("pause");
+		pausa();
 		
-	}else{
+		jugarPC=0;
+		
+	}
+	else
+		{ //si es un jugador vs la maquina...
+	
 		//pedir nombre del jugador
 		printf("\n*Ingrese el nombre del primer jugador\n->");
-		fflush(stdin); //se limpia el buffer
 		gets(jugador1);
 		
 		//pedir caracter para el jugador
 		do{
 			printf("*Ingrese el caracter a jugar entre x/o\n->");
-			fflush(stdin);
 			scanf("%c", &caracter1);
 			
 			if(caracter1!='x' && caracter1!='o')
 				printf("Solo puede elegir entra x/o para jugar...\n");
 			
 		} while(caracter1!='x' && caracter1!='o');
-
+		
+		clean_stdin();
+		
 		strcpy(jugador2, "Computadora"); //el jugador sera la comutadora por defecto
 		
 		//se le da el caracter a la computadora dependiendo de la eleccion del jugador 1
@@ -225,11 +197,20 @@ void jugar(){
 			caracter2='o';
 		else
 			caracter2='x';
+			
+		jugarPC=1;
 	}
 	
-	system("cls"); //borrar pantalla
+	//preguntar si desea jugar en pc o en papel
 	
-	fflush(stdin); //se limpia el buffer
+	do{
+		printf("\n*Desea jugar en PC o en Papel? 1=Si, 0=No\n->");
+		scanf("%i", &papel);
+	}while(papel<0 || papel>1);
+	
+	limP(); //borrar pantalla
+	
+	clean_stdin(); //se limpia el buffer
 	
 	vaciar(posOcupadasX); //se borra o inicializa en cero todas las posciones del vector
 	vaciar(posOcupadasO); //se borra o inicializa en cero todas las posciones del vector
@@ -237,26 +218,52 @@ void jugar(){
 	//***************************** Rutina del loop del Juego* ******************************************/
 	while(Jugar==1 && ganador==2){
 		
-		//Jugador 1
-		presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
+			//----------Jugador 1 ------------
+		if(papel==1){
+			ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
+			limP();
+			presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
+			printf("\n*Revisar el txt\n");
+		}else{
+			presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
+			ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
+		}
 		
-		ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
-		
+		//validacion del valor de ganador para saber si alguien gano
 		if(ganador==0 && caracter1=='x'){
 			printf("\n*Felicidades: %s has ganado!!\n\n", jugador1);
 			break;			
 		}else if(ganador==0 && caracter1=='o'){
-			printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
-			break;
+			if(jugarPC==1){
+				printf("\n*Lo siento, la comutadora ha ganado!!\n\n", jugador2);
+				break;
+			}else{
+				printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
+				break;
+			}
 		}
+		
 		if(ganador==1 && caracter1=='o'){
 			printf("\n*Felicidades: %s has ganado!!\n\n", jugador1);
 			break;
 		}else if(ganador==1 && caracter1=='x'){
-			printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
-			break;
+			if(jugarPC==1){
+				printf("\n*Lo siento, la comutadora ha ganado!!\n\n", jugador2);
+				break;
+			}else{
+				printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
+				break;
+			}
 		}
 		
+		//validacion de empate
+		if(posX+posO==9){ 
+			printf("\n*Ha sido un empate! No hay Ganador!\n\n", jugador2);
+			pausa();
+			limP();
+			break;
+		}
+	
 		//solicitud de las coordenadas del jugador 1
 		do{
 			printf("\nTurno para: %s\n",jugador1);
@@ -277,42 +284,81 @@ void jugar(){
 			posOcupadasO[posO]=posicion;
 			posO++;
 		}
+		
+		pausa();
+		limP();
+		
+		//----------Jugador 2 ------------
 
-		system("pause");
-		system("cls");
+		if(papel==1){
+			ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
+			limP();
+			presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
+			printf("\n*Revisar el txt\n");
+		}else{
+			presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
+			ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
+		}
 		
-		//Jugador 2
-		presentacion(jugador1, jugador2,caracter1, caracter2); //invocacion de la funcion que muestra la presentacion
-	
-		ganador=tablero(posOcupadasX, posOcupadasO); //invocacion de la funcion que muestra el tablero
-		
+		//validacion del valor de ganador para saber si alguien gano
 		if(ganador==0 && caracter1=='x'){
 			printf("\n*Felicidades: %s has ganado!!\n\n", jugador1);
 			break;			
 		}else if(ganador==0 && caracter1=='o'){
-			printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
-			break;
+			if(jugarPC==1){
+				printf("\n*Lo siento, la comutadora ha ganado!!\n\n", jugador2);
+				break;
+			}else{
+				printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
+				break;
+			}
 		}
 		if(ganador==1 && caracter1=='o'){
 			printf("\n*Felicidades: %s has ganado!!\n\n", jugador1);
 			break;
 		}else if(ganador==1 && caracter1=='x'){
-			printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
+			if(jugarPC==1){
+				printf("\n*Lo siento, la comutadora ha ganado!!\n\n", jugador2);
+				break;
+			}else{
+				printf("\n*Felicidades: %s has ganado!!\n\n", jugador2);
+				break;
+			}
+		}
+		
+		//validacion de empate
+		if(posX+posO==9){ 
+			printf("\n*Ha sido un empate! No hay Ganador!\n\n", jugador2);
+			pausa();
+			limP();
 			break;
 		}
 		
 		//solicitud de las coordenadas del jugador 2
-		do{
-			printf("\nTurno para: %s\n",jugador2);
-			printf("\n*Ingrese la Fila de la posicion a alegir\n->");
-			scanf("%i", &fila);
+		if(jugarPC==1){ //si se elgicio jugar vs la pc
+		
+			do{
+				fila =rand() % 3; //numeros aleatorios entre 0 y 2
+				columna =rand() % 3; //numeros aleatorios entre 0 y 2
+				
+				posicion=coordenadaAPosicion(fila, columna);
+
+			} while (posValidas(posicion, posOcupadasX, posOcupadasO, posX, posO)!=1); //ciclo que asegura que elija una posicion no ocupada
 			
-			printf("*Ingrese la Columna de la posicion a alegir\n->");
-			scanf("%i", &columna);
-			
-			posicion=coordenadaAPosicion(fila, columna);
-			
-		}while(fila<0 || columna<0 || fila>2 || columna>2 || posValidas(posicion, posOcupadasX, posOcupadasO, posX, posO)!=1); //valiadacion de las coordenadas
+		}else{ //sino..
+		
+			do{
+				printf("\nTurno para: %s\n",jugador2);
+				printf("\n*Ingrese la Fila de la posicion a alegir\n->");
+				scanf("%i", &fila);
+				
+				printf("*Ingrese la Columna de la posicion a alegir\n->");
+				scanf("%i", &columna);
+				
+				posicion=coordenadaAPosicion(fila, columna);
+				
+			}while(fila<0 || columna<0 || fila>2 || columna>2 || posValidas(posicion, posOcupadasX, posOcupadasO, posX, posO)!=1); //valiadacion de las coordenadas
+		}
 		
 		if(caracter2=='x'){
 			posOcupadasX[posX]=posicion;
@@ -321,21 +367,21 @@ void jugar(){
 			posOcupadasO[posO]=posicion;
 			posO++;
 		}
-		
-		system("pause");
-		system("cls");
+
+		pausa();
+		limP();
 		
 	}
-	//***************************** Fin del loop del Juego* ******************************************/
+	//***************************** Fin del loop del Juego *******************************************/
 	
 	printf("\n\n");
-	system("pause");
+	pausa();
 }
 
 //funcion que muestra la cabecera con los nombres
 void presentacion(char jugador1[25], char jugador2[25], char c1, char c2){
 	//presentacion
-	gotoxy(15,0); printf("%s con: %c  |VS|  %s con: %c\n\n", jugador1,c1, jugador2,c2);
+	printf("               %s con: %c  |VS|  %s con: %c\n\n", jugador1,c1, jugador2,c2);
 }
 
 //funcion del tablero 
@@ -544,8 +590,6 @@ int posValidas(int pos, int posOcupadasX[9], int posOcupadasO[9], int posX, int 
 	for (i = 0; i < posX; i++){
 
 		if (pos == posOcupadasX[i]){
-			printf("\n*La posicion elegida ya ha sido ocupada\n");
-			system("pause");
 			return 0;
 		}
 	
@@ -555,8 +599,6 @@ int posValidas(int pos, int posOcupadasX[9], int posOcupadasO[9], int posX, int 
 	for (i = 0; i < posO; i++){
 
 		if (pos == posOcupadasO[i]){
-			printf("\n*La posicion elegida ya ha sido ocupada\n");
-			system("pause");
 			return 0;
 		}
 	
@@ -738,4 +780,31 @@ void vaciar(int posOcupadas[9])
 	{
 		posOcupadas[i] = 0;
 	}
+}
+
+//limpia la pantalla
+void limP()
+{
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
+
+//pausa al sistema
+void pausa()
+{
+	printf("\n\nPresione una tecla para continuar...\n");
+	getchar();
+}
+
+//metodo que limpia el buffer
+void clean_stdin(void)
+{
+	int c;
+	do
+	{
+		c = getchar();
+	} while (c != '\n' && c != EOF);
 }
